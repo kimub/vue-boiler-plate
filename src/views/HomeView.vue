9c5b60a6cf694b4894ece384862174f5f56508d6
ubeom kim <kimub1204@gmail.com>
2024-01-16 22:21:47 +0900

    init: 개발 환경 셋팅 중..

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <h1 className="text-2xl font-bold underline">Hello world!</h1>
+  </main>
+</template>
