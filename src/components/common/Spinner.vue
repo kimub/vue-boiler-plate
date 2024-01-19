14c2bd7d47edacfdf9419f65ab0c08abcee34a8c
ub <kimub1204@gmail.com>
2024-01-19 12:53:35 +0900

    init: 개발 환경 셋팅 중..

---
+++ src/components/common/Spinner.vue
@@ -0,0 +1,9 @@
+<template>
+  <VueSpinner size="32" color="#aa8df6" />
+</template>
+
+<script setup lang="ts">
+import { VueSpinner } from 'vue3-spinners';
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,40 @@
 <template>
-  {{ data }}
+  <div v-show="isLoading">
+    <Spinner />
+  </div>
+  <div v-show="!isLoading">
+    {{ data }}
+  </div>
   <button @click="onClick">sadas</button>
   <div class="text-3xl text-slate-400 md:text-red-500">Home</div>
 </template>
 
 <script setup lang="ts">
+import Spinner from '@/components/common/Spinner.vue';
 import { useFetch } from '@/composables/useFetch';
 import { useMutation } from '@/composables/useMutation';
 
-const { data, isLoading, isSuccess, isError, refetch } = useFetch('todos', {});
-const { mutate } = useMutation('/posts', {});
+const { data, isLoading, isSuccess, isError, refetch } = useFetch('todos', {
+  onSuccess: (res) => {
+    console.log('success!');
+    console.log(res);
+  },
+  onError: (err) => {
+    console.error('error!');
+    console.log(err);
+  }
+});
+
+const { mutate } = useMutation('/posts', {
+  onSuccess: (res) => {
+    console.log('success!');
+    console.log(res);
+  },
+  onError: (err) => {
+    console.error('error!');
+    console.log(err);
+  }
+});
 
 const onClick = () => {
   mutate({
